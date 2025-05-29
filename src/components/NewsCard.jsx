422b9cd6d179be740773c7a494e4d61913fd4345
mdayan96386 <ayan96386@gmail.com>
2025-05-29 14:07:56 +0530

    image fix

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -17,7 +17,7 @@ function NewsCard({ news }) {
         <img
           className="h-56 w-56"
           src={
-            news.urlToImage ||
+            news.image ||
             "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ="
           }
           alt=""
